817cc6ae30b69aa8352329ca000b479ee71f6dfc
yjesp <yjesp@hanmail.net>
2024-01-21 19:56:27 +0900

    Add files via upload

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,12 +1,3 @@
-/* using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop02 World!");
-    }
-} */
 
 using System;
 using System.Collections.Generic;
